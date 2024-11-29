dc0a0880f52641085761c8a6770102eefddd6925
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-11-29 11:24:11 +0000

    Reformat test

--- test/app/plain_ts/index.ts
+++ test/app/plain_ts/index.ts
@@ -1,8 +1,8 @@
-import { Component, input } from "@angular/core";
+import {Component, input} from '@angular/core';
 
 @Component({
-  selector: "my-comp",
-  template: "<div></div>",
+  selector: 'my-comp',
+  template: '<div></div>',
 })
 export class MyComponent {
   name = input.required<string>();
