cc9d43036a73a2a0a781a5f32f0ca61192e329ce
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-05-06 12:01:30 +0000

    Cleanup of unused bundling

--- src/worker/rollup.config.mjs
+++
@@ -1,8 +0,0 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import terser from '@rollup/plugin-terser';
-
-export default {
-  external: ['typescript', '@angular/compiler-cli'],
-  plugins: [commonjs(), nodeResolve(), terser()],
-};
