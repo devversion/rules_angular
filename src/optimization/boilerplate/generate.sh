b5539bdf37be91ab6384f04ff0aaebdd6660f30c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-05-22 19:14:30 +0000

    Support files outside of current package.
    This is useful for e.g. benchmark tests in Angular FW that share
    utilities from outside the current Bazel package.

--- src/optimization/boilerplate/generate.sh
+++ src/optimization/boilerplate/generate.sh
@@ -15,6 +15,7 @@ function suppress_on_success {
 # find path to the Angular CLI executable.
 RUNFILES="$(realpath $0.runfiles)"
 NG_CLI_TOOL="$RUNFILES/rules_angular/src/optimization/ng_cli_tool_/ng_cli_tool"
+YQ_BIN=$(realpath $YQ_BIN)
 
 # cd into the bazel bin dir
 cd ${OUT_DIR}/..
@@ -27,7 +28,15 @@ suppress_on_success $NG_CLI_TOOL new boilerplate --skip-install --skip-git --ski
 
 # remove boilerplate app component code
 rm boilerplate/src/app/* -Rf
+rm boilerplate/src/main.ts
 
-# Remove zone.js polyfill. We expect all dependencies to be explicitly
-# going through the "main" entry-point.
-sed -i s/\"zone.js\"// boilerplate/angular.json
+# Adjust `tsconfig.app.json`.
+# Remove json5 comments first, and then add property via yq.
+cat boilerplate/tsconfig.app.json \
+   | sed 's/^\/.*//' \
+   | tee boilerplate/tsconfig.tmp.json &> /dev/null
+
+$YQ_BIN -o=json '.compilerOptions.preserveSymlinks = true' \
+   -i boilerplate/tsconfig.tmp.json
+
+mv boilerplate/tsconfig.tmp.json boilerplate/tsconfig.app.json
