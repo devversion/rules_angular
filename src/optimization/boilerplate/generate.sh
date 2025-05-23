e820ef96082bcf07e432f40a1aa0fdd4cf5f4412
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-05-23 12:27:19 +0000

    Cleanup unnecessary logic and fix optimize test

--- src/optimization/boilerplate/generate.sh
+++ src/optimization/boilerplate/generate.sh
@@ -15,7 +15,6 @@ function suppress_on_success {
 # find path to the Angular CLI executable.
 RUNFILES="$(realpath $0.runfiles)"
 NG_CLI_TOOL="$RUNFILES/rules_angular/src/optimization/ng_cli_tool_/ng_cli_tool"
-YQ_BIN=$(realpath $YQ_BIN)
 
 # cd into the bazel bin dir
 cd ${OUT_DIR}/..
@@ -24,19 +23,8 @@ cd ${OUT_DIR}/..
 suppress_on_success $NG_CLI_TOOL new boilerplate --skip-install --skip-git --skip-tests
 
 # disable caching as this runs in the sandbox..
-(cd boilerplate && $NG_CLI_TOOL cache off && $NG_CLI_TOOL analytics off)
+(cd boilerplate && $NG_CLI_TOOL cache off && $NG_CLI_TOOL analytics off) &> /dev/null
 
 # remove boilerplate app component code
 rm boilerplate/src/app/* -Rf
 rm boilerplate/src/main.ts
-
-# Adjust `tsconfig.app.json`.
-# Remove json5 comments first, and then add property via yq.
-cat boilerplate/tsconfig.app.json \
-   | sed 's/^\/.*//' \
-   | tee boilerplate/tsconfig.tmp.json &> /dev/null
-
-$YQ_BIN -o=json '.compilerOptions.preserveSymlinks = true' \
-   -i boilerplate/tsconfig.tmp.json
-
-mv boilerplate/tsconfig.tmp.json boilerplate/tsconfig.app.json
