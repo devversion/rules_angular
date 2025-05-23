3539898f6aa6e9c990fd43fb8d4aa3fe8c9a1456
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-05-23 07:47:35 +0000

    fix: minor improvments to the `optimize.sh` and `generate.sh`

--- src/optimization/boilerplate/generate.sh
+++ src/optimization/boilerplate/generate.sh
@@ -10,10 +10,10 @@ NG_CLI_TOOL="$RUNFILES/rules_angular/src/optimization/ng_cli_tool_/ng_cli_tool"
 cd ${BAZEL_BINDIR}/src/optimization/boilerplate
 
 # generate boilerplate
-$NG_CLI_TOOL new boilerplate --skip-install --skip-git
+$NG_CLI_TOOL new boilerplate --skip-install --skip-git --skip-tests
 
 # disable caching as this runs in the sandbox..
-(cd boilerplate && $NG_CLI_TOOL cache disable)
+(cd boilerplate && $NG_CLI_TOOL cache off && $NG_CLI_TOOL analytics off)
 
 # remove boilerplate app component code
 rm boilerplate/src/app/* -Rf
