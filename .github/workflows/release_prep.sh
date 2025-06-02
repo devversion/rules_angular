2f312336b61db40ea71db884058ff3306583523c
Alex Eagle <alex@aspect.dev>
2025-06-02 11:55:39 -0700

    chore: add bazel-central-registry publishing
    Allows Bazel users to get rules_angular from their MODULE.bazel file

---
+++ .github/workflows/release_prep.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+
+# Argument provided by reusable workflow caller, see
+# https://github.com/bazel-contrib/.github/blob/d197a6427c5435ac22e56e33340dff912bc9334e/.github/workflows/release_ruleset.yaml#L72
+TAG=$1
+# The prefix is chosen to match what GitHub generates for source archives
+# This guarantees that users can easily switch from a released artifact to a source archive
+# with minimal differences in their code (e.g. strip_prefix remains the same)
+PREFIX="rules_angular-${TAG:1}"
+ARCHIVE="rules_angular-$TAG.tar.gz"
+
+# NB: configuration for 'git archive' is in /.gitattributes
+git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
+SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
+
+cat << EOF
+## Using Bzlmod with Bazel 6 or greater
+
+\`\`\`starlark
+bazel_dep(name = "rules_angular", version = "${TAG:1}")
+\`\`\`
+EOF
